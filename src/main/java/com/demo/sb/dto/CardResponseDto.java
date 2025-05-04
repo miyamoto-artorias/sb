900040cc0172f7f654be27523cda7aae471f22f4
yassin <mohamedyassinbenchayada98@gmail.com>
2025-05-04 14:06:50 +0100

    added cardrNumber in getUserCard

--- src/main/java/com/demo/sb/dto/CardResponseDto.java
+++ src/main/java/com/demo/sb/dto/CardResponseDto.java
@@ -9,15 +9,17 @@ public class CardResponseDto {
     private boolean valid;
     private float balance;
     private int userId;
+    private String cardNumber; // New field for card number
 
     public static CardResponseDto fromEntity(Card card) {
         CardResponseDto dto = new CardResponseDto();
         dto.setId(card.getId());
         dto.setValid(card.isValid());
         dto.setBalance(card.getBalance());
+        dto.setCardNumber(card.getCardNumber()); // Include cardNumber
         if (card.getUser() != null) {
             dto.setUserId(card.getUser().getId());
         }
         return dto;
     }
-} 
+}
